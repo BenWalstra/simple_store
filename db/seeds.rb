1d02cdc22f7e899e5173d4402ab35da3d4aec99b
BenWalstra <benwalstra@gmail.com>
2019-10-13 20:49:53 -0500

    seeded data into products table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+Product.destroy_all
+
+676.times do
+  Product.create(
+    title: Faker::Commerce.unique.product_name,
+    price: Faker::Commerce.price,
+    stock_quantity: Faker::Number.within(range: 10..1000)
+  )
+end
+
+puts "Generated #{Product.count} appointments"
